35e188043900368a7e7b950be9c05a9b81cfcb2f
Lucas Aguiar <7lucasdaniel@gmail.com>
2025-01-18 20:56:25 -0300

    solving roman to integer

---
+++ Easy/RomanToIntegerExercice.cs
@@ -0,0 +1,45 @@
+﻿//URL: https://leetcode.com/problems/roman-to-integer/
+
+public class RomanToIntegerExercice
+{
+    public enum RomanSymbols
+    {
+        I = 1,
+        V = 5,
+        X = 10,
+        L = 50,
+        C = 100,
+        D = 500,
+        M = 1000,
+    }
+
+    public int RomanToInt(string s)
+    {
+        var sumTotal = 0;
+        int previousValue = 0;
+        int memoryCell = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var currentValue = (int)Enum.Parse(typeof(RomanSymbols), $"{s[i]}");
+
+            if (previousValue > currentValue && memoryCell != 0)
+            {
+                sumTotal += memoryCell;
+                memoryCell = 0;
+            }
+            if (previousValue == currentValue || memoryCell == 0)
+            {
+                memoryCell += currentValue;
+            }
+            if (previousValue < currentValue && i > 0)
+            {
+                sumTotal += (currentValue - memoryCell);
+                memoryCell = 0;
+            }
+            previousValue = currentValue;
+        }
+
+        return sumTotal + memoryCell;
+    }
+}
---
+++ LeetCode.Tests/Difficults/Easy/RomanToIntTest.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Tests.Difficults.Easy
+{
+    public class RomanToIntTest
+    {
+        [Fact]
+        public void RomanToInt()
+        {
+            // Arrange
+            RomanToIntegerExercice romanToIntergerExercice = new RomanToIntegerExercice();
+
+            // Act
+            var result_1 = romanToIntergerExercice.RomanToInt("IV");
+            var result_2 = romanToIntergerExercice.RomanToInt("III");
+            var result_3 = romanToIntergerExercice.RomanToInt("LVIII");
+            var result_4 = romanToIntergerExercice.RomanToInt("MCMXCIV");
+
+            // Assert
+            Assert.Equal(4, result_1);
+            Assert.Equal(3, result_2);
+            Assert.Equal(58, result_3);
+            Assert.Equal(1994, result_4);
+        }
+    }
+}
