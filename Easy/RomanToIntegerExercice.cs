0f886c9c1fd223097830fc3abf02dca88280589e
Lucas Aguiar <7lucasdaniel@gmail.com>
2025-01-18 22:53:57 -0300

    adding a better way to do this

--- Easy/RomanToIntegerExercice.cs
+++ Easy/RomanToIntegerExercice.cs
@@ -37,3 +37,42 @@ public class RomanToIntegerExercice
         return total;
     }
 }
+
+// This is a better way to manage memory and speed.
+public class Solution
+{
+    private static readonly int[] dic = new int[26];
+
+    static Solution()
+    {
+        dic['I' - 'A'] = 1;
+        dic['V' - 'A'] = 5;
+        dic['X' - 'A'] = 10;
+        dic['L' - 'A'] = 50;
+        dic['C' - 'A'] = 100;
+        dic['D' - 'A'] = 500;
+        dic['M' - 'A'] = 1000;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public int RomanToInt(string s)
+    {
+        var total = 0;
+        int lastValue = 0;
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            var currentValue = dic[s[i] - 'A'];
+            total += currentValue;
+
+            if (currentValue < lastValue)
+            {
+                total -= (currentValue * 2);
+            }
+
+            lastValue = currentValue;
+        }
+
+        return total;
+    }
+}
