eec65aa87f815daa35156adbb0b70b06ef651e06
Lucas Aguiar <7lucasdaniel@gmail.com>
2025-06-23 13:22:21 -0300

    feat: merge two sorted lists

---
+++ Easy/MergeTwoSortedListsExercice.cs
@@ -0,0 +1,29 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class MergeTwoSortedListsExercice
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        if (list1 is null) return list2;
+        if (list2 is null) return list1;
+
+        if (list1.val <= list2.val)
+        {
+            list1.next = MergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        else
+        {
+            list2.next = MergeTwoLists(list2.next, list1);
+            return list2;
+        }
+    }
+}
---
+++ LeetCode.Tests/Difficults/Easy/MergeTwoSortedListsTest.cs
@@ -0,0 +1,36 @@
+﻿using Easy;
+
+namespace LeetCode.Tests.Difficults.Easy
+{
+    public class MergeTwoSortedListsTest
+    {
+        [Fact]
+        public void MergeTwoLists()
+        {
+            // Arrange
+            MergeTwoSortedListsExercice validParenthesesExercice = new MergeTwoSortedListsExercice();
+
+            var list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+            var list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+            var merged = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4))))));
+
+            // Act
+            var test_1 = validParenthesesExercice.MergeTwoLists(list1, list2);
+
+            // Assert
+            Assert.True(AreEqual(merged, test_1));
+        }
+
+        private static bool AreEqual(ListNode a, ListNode b)
+        {
+            while (a != null && b != null)
+            {
+                if (a.val != b.val) return false;
+                a = a.next;
+                b = b.next;
+            }
+            return a == null && b == null;
+        }
+
+    }
+}
