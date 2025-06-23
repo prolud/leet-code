b29547fe970dd460afe24b13c5f0c9d9b69e9d6c
Lucas Aguiar <7lucasdaniel@gmail.com>
2025-06-23 10:36:26 -0300

    ValidParentheses

---
+++ LeetCode.Tests/Difficults/Easy/ValidParenthesesTest.cs
@@ -0,0 +1,32 @@
+﻿using Easy;
+
+namespace LeetCode.Tests.Difficults.Easy
+{
+    public class ValidParenthesesTest
+    {
+        [Fact]
+        public void ValidParentheses()
+        {
+            // Arrange
+            ValidParenthesesExercice validParenthesesExercice = new ValidParenthesesExercice();
+
+            // Act
+            var test_1 = validParenthesesExercice.IsValid("(){}[]");
+
+            var test_2 = validParenthesesExercice.IsValid("({})");
+
+            var test_3 = validParenthesesExercice.IsValid("([)]");
+
+            var test_4 = validParenthesesExercice.IsValid("()]");
+
+            var test_5 = validParenthesesExercice.IsValid("[([]])");
+
+            // Assert
+            Assert.True(test_1);
+            Assert.True(test_2);
+            Assert.False(test_3);
+            Assert.False(test_4);
+            Assert.False(test_5);
+        }
+    }
+}
