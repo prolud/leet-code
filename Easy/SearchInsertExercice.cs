1f689f5b53b695f73b4c899f394f9e175292bd36
Lucas Aguiar <7lucasdaniel@gmail.com>
2025-07-03 13:18:56 -0300

    exercices solve

---
+++ Easy/SearchInsertExercice.cs
@@ -0,0 +1,15 @@
+﻿namespace Easy;
+
+public class SearchInsertExercice
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int correctIndex;
+        for (correctIndex = 0; correctIndex < nums.Length && target >= nums[correctIndex]; correctIndex++)
+        {
+            if (nums[correctIndex] == target) return correctIndex;
+        }
+
+        return correctIndex;
+    }
+}
---
+++ LeetCode.Tests/Difficults/Easy/StrStrTest copy.cs
@@ -0,0 +1,22 @@
+﻿using Easy;
+
+namespace LeetCode.Tests.Difficults.Easy
+{
+    public class SearchInsertTest
+    {
+        [Fact]
+        public void SearchInsert()
+        {
+            // Arrange
+            SearchInsertExercice validParenthesesExercice = new SearchInsertExercice();
+
+            // Act
+            var test_1 = validParenthesesExercice.SearchInsert([1,3,5,6], 5);
+            var test_2 = validParenthesesExercice.SearchInsert([1,3,5,6], 2);
+
+            // Assert
+            Assert.Equal(2, test_1);
+            Assert.Equal(1, test_2);
+        }
+    }
+}
